95705aca91474c843f56a23f1dd819f1522ab8fc
Shiraz Shay <shirazs054@gmail.com>
2022-04-17 21:52:48 +0300

    add unionFind

---
+++ mst-algorithms/mst-algorithms/UnionFind.hpp
@@ -0,0 +1,62 @@
+//
+//  UnionFind.hpp
+//  mst-algorithms
+//
+//  Created by Shiraz Shay on 17/04/2022.
+//
+
+#ifndef UnionFind_hpp
+#define UnionFind_hpp
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+struct UnionFindNode {
+    int parent;
+    int size;
+};
+
+class UnionFind {
+    
+    int arrSize;
+    UnionFindNode *arr;
+
+public:
+    
+    UnionFind(int size)
+    {
+        arr = (UnionFindNode*)malloc(size * sizeof(UnionFindNode));
+        this->arrSize = size;
+    }
+
+    void MakeSet(int i)
+    {
+        arr[i].parent = i;
+        arr[i].size = 1;
+        
+    }
+    
+    int Parent(int x){
+        return arr[x].parent;
+    }
+     
+    // Finds with path compression
+    int Find(int x);
+    //Union by size
+    void Union(int x, int y);
+    
+    void PrintForest() ;
+    void PrintNode(int x);
+
+
+};
+
+
+
+
+
+
+
+
+#endif /* UnionFind_hpp */
--- mst-algorithms/mst-algorithms/main.cpp
+++ mst-algorithms/mst-algorithms/main.cpp
@@ -4,42 +4,33 @@
 //
 //  Created by Yinon Horev on 13/04/2022.
 //
-using namespace std;
 
 #include <iostream>
 #include "graph.hpp"
+#include "UnionFind.hpp"
+using namespace std;
 
 
-void display_AdjList(adjNode* ptr, int i);
 
 int main(int argc, const char * argv[]) {
-    // graph edges array.
-    graphEdge edges[] = {
-        // (x, y, w) -> edge from x to y with weight w
-        {0,1,2},{0,2,4},{1,4,3},{2,3,2},{3,1,4},{4,3,3}
-    };
-    int N = 6;      // Number of vertices in the graph
-    // calculate number of edges
-    int n = sizeof(edges)/sizeof(edges[0]);
-    // construct graph
-    DiaGraph diagraph(edges, n, N);
-    // print adjacency list representation of graph
-    cout<<"Graph adjacency list "<<endl<<"(start_vertex, end_vertex, weight):"<<endl;
-    for (int i = 0; i < N; i++)
-    {
-        // display adjacent vertices of vertex i
-        display_AdjList(diagraph.head[i], i);
-    }
+    int n = 7;
+    UnionFind test(n);
+    for (int i=0; i <= n; i++) {
+          test.MakeSet(i);
+      }
+      
+    test.Union(0, 1);
+    test.PrintNode(0);
+    test.PrintNode(1);
+    test.Union(1, 2);
+    test.PrintNode(0);
+    test.PrintNode(1);
+    test.PrintNode(2);
+    test.Union(2, 3);
+    test.PrintForest();
+    cout << test.Find(3) << endl;
+    test.PrintForest();
     return 0;
+
 }
 
-// print all adjacent vertices of given vertex
-void display_AdjList(adjNode* ptr, int i)
-{
-    while (ptr != nullptr) {
-        cout << "(" << i << ", " << ptr->val
-            << ", " << ptr->cost << ") ";
-        ptr = ptr->next;
-    }
-    cout << endl;
-}
