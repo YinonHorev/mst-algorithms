f9dbaadb808e54bec76ebf9561b1f710eeb51b59
Yinon Horev <yinon.horev@getfabric.com>
2022-04-18 19:43:27 +0300

    added heap from web

--- mst-algorithms/mst-algorithms/BinaryHeap.hpp
+++ mst-algorithms/mst-algorithms/BinaryHeap.hpp
@@ -8,6 +8,31 @@
 #ifndef BinaryHeap_hpp
 #define BinaryHeap_hpp
 
-#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <iterator>
+using namespace std;
+/*
+ * Class Declaration
+ */
+class Heap
+{
+    private:
+        vector <int> heap;
+        int left(int parent);
+        int right(int parent);
+        int parent(int child);
+        void heapifyup(int index);
+        void heapifydown(int index);
+    public:
+        Heap()
+        {}
+        void Insert(int element);
+        void DeleteMin();
+        int ExtractMin();
+        void DisplayHeap();
+        int Size();
+};
 
 #endif /* BinaryHeap_hpp */
